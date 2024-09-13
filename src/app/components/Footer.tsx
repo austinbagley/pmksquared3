de6b45dc2c4bb11436b2c9ff54c592cac03bd1b5
austinbagley <austinbagley@austinbgleysmbp.lan>
2024-09-13 15:45:41 -0600

    initial flexbox based styling

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="container mx-auto grid bg-white sm:min-h-96 sm:rounded-b-xl p-4 sm:shadow-xl sm:mb-20">
+      This is the footer
+    </footer>
+  )
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header className="container mx-auto bg-white p-4 sm:shadow-xl content-center">
+      <h1 className="text-2xl">This is the header</h1>
+    </header>
+  )
+}
